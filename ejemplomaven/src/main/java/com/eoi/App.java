9784a1fef81e7b76bdc93eec76fa704669d749b2
Benjamin Romero <benjamin.romero.job@gmail.com>
2022-10-24 18:01:25 +0200

    comienzo

---
+++ ejemplomaven/src/main/java/com/eoi/App.java
@@ -0,0 +1,12 @@
+package com.eoi;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Person persona1 = new Person("Paco", 29);
+        System.out.println(persona1.getNombre() + " - " + persona1.getEdad());
+        
+    }
+}
